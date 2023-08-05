5d1a2f97e62c82eae7650ec5abda3d9d3e43c601
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-08-05 11:32:42 +0400

    delete env from git

--- storyBook/routes/stories.js
+++ storyBook/routes/stories.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const { ensureAuth, ensureGuest} = require('../middleware/authMiddleware')
+const { ensureAuth } = require('../middleware/authMiddleware')
 
 const Story = require('../models/Story')
 
